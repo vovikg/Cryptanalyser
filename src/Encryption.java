9ccdb6f35626cf86d7a1d69064731de5a20d5b65
Vladimir Gluschivschi <bratsashy@gmail.com>
2022-05-15 21:05:19 +0100

    moved I/O to class: Encryption

--- src/Encryption.java
+++ src/Encryption.java
@@ -1,6 +1,34 @@
-
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.util.ArrayList;
 
 public class Encryption {
-    public Encryption(String inputFile, String output, int key) {
+    ArrayList<Character> sourceTextArray;
+
+    public Encryption() {
+        sourceTextArray = new ArrayList<>();
+
+        try (InputStream inputStream = Files.newInputStream(Main.sourcePath)) {
+        int c;
+        while ((c = inputStream.read()) != -1) {
+
+
+            sourceTextArray.add((char) c);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+//        System.out.println(sourceTextArray); // Just for testing
+    }
+
+
+
+
+    public ArrayList encrypt(ArrayList inputFile, ArrayList output, int key){
+        return null;
     }
 }
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,15 @@
-import java.io.*;
-
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 
-
 public class Main {
+    static Path sourcePath = Path.of("/Users/vladimirgluschivschi/Documents/JavaTasks/source.txt");;
+    static Path destinationPath = Path.of("/Users/vladimirgluschivschi/Documents/JavaTasks/destination.txt");;
+
     public static void main(String[] args) {
-        ArrayList<Character> list = new ArrayList<>();
-        Path sourcePath = Path.of("/Users/vladimirgluschivschi/Documents/JavaTasks/source.txt");
-        Path destinationPath = Path.of("/Users/vladimirgluschivschi/Documents/JavaTasks/destination.txt");
+        Encryption encryption = new Encryption();
+//        Decryption decryption = new Decryption();
+//        Cryptanalyser cryptanalyser = new Cryptanalyser();
 
-
-        try(InputStream inputStream = Files.newInputStream(sourcePath);
-        OutputStream outputStream = Files.newOutputStream(destinationPath))
-        {
-
-            int c;
-            while((c=inputStream.read())!=-1){
-
-                System.out.print((char)c);
-                list.add((char)c);
-            }
-        }
-        catch(IOException ex){
-
-            System.out.println(ex.getMessage());
-        }
 
 
     }
