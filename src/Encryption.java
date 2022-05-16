3dac3aca7ea242cc313f99c13e976872888a87d3
Vladimir Gluschivschi <bratsashy@gmail.com>
2022-05-16 21:49:41 +0100

    added method: encrypt

--- src/Encryption.java
+++ src/Encryption.java
@@ -1,10 +1,14 @@
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Encryption {
-    ArrayList<Character> sourceTextArray;
+    private ArrayList<Character> sourceTextArray;
+    private char alphabetArray[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'x',
+            '1','2','3','4','5','6','7','8','9','0','!','@','£','$','%','(',')','?','\\','.',','};
 
     public Encryption() {
         sourceTextArray = new ArrayList<>();
@@ -26,8 +30,20 @@ public class Encryption {
 
 
 
-    public ArrayList encrypt(ArrayList inputFile, ArrayList output, int key){
-        return null;
+    public void encrypt( int key){
+        ArrayList<Character> destArray = new ArrayList<>();
+
+
+
+        for(int i =0; i <= sourceTextArray.size(); i ++){
+
+            destArray.add(i, sourceTextArray.get(i+key));
+
+
+
+            }
+        System.out.println("DestArray = :"+ destArray);
+
     }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,10 @@ public class Main {
 
     public static void main(String[] args) {
         Encryption encryption = new Encryption();
-//        Decryption decryption = new Decryption();
-//        Cryptanalyser cryptanalyser = new Cryptanalyser();
+        Decryption decryption = new Decryption();
+        Cryptanalyser cryptanalyser = new Cryptanalyser();
+
+        encryption.encrypt(3);
 
 
 
