3cf477f5021849e0dd69816eded70297bbae7ed0
Vladimir Gluschivschi <bratsashy@gmail.com>
2022-06-02 13:33:56 +0100

    redone gitignore file

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,10 @@ public class Main {
     public static void main(String[] args) {
         Encryption encryption = new Encryption();
 
-        encryption.encrypt("files/source.txt", "files/destination.txt", 3);
+        encryption.encrypt("files/source.txt", "files/destination.txt",3);
         encryption.decryption("files/destination.txt", "files/decryptedFile.txt", 3);
 
-        encryption.cryptAnalyzer("files/destination.txt", "files/CryptAnalyserDestinationFile.txt");
+//        encryption.cryptAnalyzer("files/destination.txt","files/CryptAnalyserDestinationFile.txt");
 
     }
 }
